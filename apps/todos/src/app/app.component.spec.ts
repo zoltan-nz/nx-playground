18fe70c5ec4e2398132472ca83901eee0c3c42b6
Zoltan <zdebre@gmail.com>
2019-03-06 15:59:00 +1300

    Add test for click event

--- apps/todos/src/app/app.component.spec.ts
+++ apps/todos/src/app/app.component.spec.ts
@@ -10,23 +10,40 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
+    const app: AppComponent = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'todos'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    console.log(app);
-    expect(app.todos).toEqual([{"title": "Todo 1"}, {"title": "Todo 2"}]);
+    const app: AppComponent = fixture.debugElement.componentInstance;
+    expect(app.todos).toEqual([{ title: 'Todo 1' }, { title: 'Todo 2' }]);
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Todos'
-    );
+    expect(compiled.querySelector('h1').textContent).toContain('Todos');
+  });
+
+  it('#addTodo() should add a todo item to #todos', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app: AppComponent = fixture.debugElement.componentInstance;
+    expect(app.todos.length).toEqual(2);
+    app.addTodo();
+    expect(app.todos.length).toEqual(3);
+  });
+
+  it('should render a button which triggers #addTodo', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const app: AppComponent = fixture.debugElement.componentInstance;
+    const compiled: HTMLDivElement = fixture.debugElement.nativeElement;
+
+    expect(app.todos.length).toEqual(2);
+
+    compiled.querySelector('button').click();
+    expect(app.todos.length).toEqual(3);
   });
 });
