4a9a6828793e261a87013bc20f0a8413c423cf66
Zoltan <zdebre@gmail.com>
2019-03-10 20:28:00 +1300

    Extract data interface to an external lib

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@nestjs/common';
-
-interface Todo {
-  title: string;
-}
+import { Todo } from '@nx-playground/data';
 
 @Injectable()
 export class AppService {
--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-interface Todo {
-  title: string;
-}
+import { Todo } from '@nx-playground/data';
 
 @Component({
   selector: 'nx-playground-root',
